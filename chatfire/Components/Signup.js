d0c1be13dcbea13a72ac50641d163138fa3917fe
Raja Sambasivam <r_sambasivam@cognitiveoptimiser.com>
2023-12-22 13:29:50 +0530

    feat:Create signup page and started

---
+++ chatfire/Components/Signup.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <View>
+      <Text>Signup Chatfire</Text>
+    </View>
+  )
+}
+
+export default Signup
